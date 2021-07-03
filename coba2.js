a6929cd26b5b0f17c44a5d00bc4e047e581c0186
verizal78 <smkn2pp2018@gmail.com>
2021-07-03 09:23:29 +0700

    first upload

---
+++ coba2.js
@@ -0,0 +1,11 @@
+const readline = require('readline-sync');
+
+
+console.info('Program penambahan angka');
+const number1 = readline.question('Masukkan angka pertama: ');
+const number2 = readline.question('Masukkan angka kedua: ');
+function add(a, b) { return a + b;
+}
+
+
+const hasil = add(parseInt(number1), parseInt(number2)); console.log(hasil);
